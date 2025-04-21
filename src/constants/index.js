091a45458e988fdc7840894e2bb3319b2e91e9e2
michael youssef <michaelyoussef396@gmail.com>
2025-04-21 22:50:01 +1000

    added 3d room and animated hero section

--- src/constants/index.js
+++ src/constants/index.js
@@ -10,6 +10,14 @@ const words = [
     { text: "Code", imgPath: "/images/code.svg" },
 ];
 
+const counterItems = [
+    { value: 15, suffix: "+", label: "Years of Experience" },
+    { value: 200, suffix: "+", label: "Satisfied Clients" },
+    { value: 108, suffix: "+", label: "Completed Projects" },
+    { value: 90, suffix: "%", label: "Client Retention Rate" },
+];
+
 export {
-    words
+    words,
+    counterItems,
 }
