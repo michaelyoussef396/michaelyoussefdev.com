afd69a0463d517385946f8bb44441a89c9a2627c
michael youssef <michaelyoussef396@gmail.com>
2025-04-23 20:41:18 +1000

    created layout for exprience section

--- src/sections/ExperienceSection.jsx
+++ src/sections/ExperienceSection.jsx
@@ -1,11 +1,25 @@
 import React from 'react'
 import TitleHeader from '../components/TitleHeader'
+import { expCards } from '../constants'
+import { div } from 'three/tsl'
 
 const ExperienceSection = () => {
   return (
     <section id='experience' className='w-full md:mt-40 mt-20 section-padding xl:px-0'>
       <div className='w-full h-full md:px-20 px-5'>
         <TitleHeader title="Professional Work Experience" sub="💼 My Career Overflow" />
+
+        <div className='mt-32 relative'>
+            <div className='relative z-60 xl:space-y-32 space-y-10'>
+                {expCards.map((card) => (
+                    <div className='exp-card-wrapper'>
+                        {card.title}
+                    </div>
+
+                ))}
+            </div>
+            
+        </div>
       </div>
     </section>
   )
