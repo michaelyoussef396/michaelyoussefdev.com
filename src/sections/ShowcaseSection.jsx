6c773514d6e55460658e8edc3f0ea29cae555b59
michael youssef <michaelyoussef396@gmail.com>
2025-04-23 17:39:47 +1000

    created right side of the bento grid

--- src/sections/ShowcaseSection.jsx
+++ src/sections/ShowcaseSection.jsx
@@ -16,7 +16,24 @@ const ShowcaseSection = () => {
             </div>
 
           </div>
+
           {/* RIGHT */}
+          <div className='project-list-wrapper overflow-hidden'>
+            <div className='project'>
+              <div className='image-wrapper bg-[#ffefdb]'>
+                <img src="images/project2.png" alt="Library Management Platform" />
+              </div>
+              <h2>Library Management Platform</h2>
+            </div>
+
+            <div className='project'>
+              <div className='image-wrapper bg-[#ffe7db]'>
+                <img src="images/project3.png" alt="YC Directory" />
+              </div>
+              <h2>YC Directory - A Startup Showcase App</h2>
+            </div>
+          </div>
+          
         </div>
       </div>
       
